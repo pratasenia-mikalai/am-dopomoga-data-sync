37b6e6bec8ceb6dad9dfce011b3a95ceb7a27d03
mpratasenia <pratasenia.mikalai@gmail.com>
2024-07-11 18:22:45 +0400

    Add job start time parameter

--- src/main/java/am/dopomoga/aidtools/batch/BatchJobParameters.java
+++ src/main/java/am/dopomoga/aidtools/batch/BatchJobParameters.java
@@ -2,6 +2,7 @@ package am.dopomoga.aidtools.batch;
 
 public interface BatchJobParameters {
 
+    String JOB_START_TIME = "job_start_time";
     String AIRTABLE_BASE_ID = "airtable_base_id";
     String ACTUAL_START_DATE = "actual_start_date";
     String AIRTABLE_NEXT_BASE_ID = "airtable_next_base_id";
--- src/main/java/am/dopomoga/aidtools/controller/JobController.java
+++ src/main/java/am/dopomoga/aidtools/controller/JobController.java
@@ -11,6 +11,7 @@ import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 
 @RequiredArgsConstructor
@@ -24,13 +25,17 @@ public class JobController {
 
     @RequestMapping(method = RequestMethod.POST, value = "/import")
     public ResponseEntity<?> importJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
-        jobLauncher.run(importJob, new JobParameters(new HashMap<>()));
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString(BatchJobParameters.JOB_START_TIME, LocalDateTime.now().toString())
+                .toJobParameters();
+        jobLauncher.run(importJob, jobParameters);
         return ResponseEntity.ok().build();
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/export")
     public ResponseEntity<?> exportJob(@RequestBody ExportJobRequest request) throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
         JobParameters jobParameters = new JobParametersBuilder()
+                .addString(BatchJobParameters.JOB_START_TIME, LocalDateTime.now().toString())
                 .addString(BatchJobParameters.AIRTABLE_BASE_ID, request.airtableBaseId())
                 .toJobParameters();
         jobLauncher.run(exportJob, jobParameters);
